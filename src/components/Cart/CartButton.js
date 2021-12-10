882833df00f85c4492c626b448c08adaf96dde34
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-10 15:02:22 -0500

    basic shopping app for redux practice

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,26 @@
+import {useDispatch} from 'react-redux';
+import { useSelector } from 'react-redux';
+import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const cartQty = useSelector(state => state.cart.totalQtyItems);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button 
+    className={classes.button}
+    onClick={toggleCartHandler}
+    >
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQty}</span>
+    </button>
+  );
+};
+
+export default CartButton;
---
+++ src/store/cart-slice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQtyItems: 0
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(
+        item => item.id === newItem.id
+      );
+      state.totalQtyItems++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          qty: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        });
+      } else {
+        existingItem.qty++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(
+        item => item.id === id
+      );
+      state.totalQtyItems--;
+      if (existingItem.qty === 1) {
+        state.items = state.items.filter(
+          item => item.id !== id
+        );
+      } else {
+        existingItem.qty--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
